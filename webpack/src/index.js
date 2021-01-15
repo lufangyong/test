cec22c8db6fb199ad49c42dad3c640e38639aaa6
lufangyong <lu1329@qq.com>
2021-01-15 15:25:55 +0800

    feat: add babel bundle

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,3 +1,6 @@
-import { foo, log } from './foo.js';
-import  './styles/index.css';
-log(foo);
+// import { foo, log } from './foo.js';
+// import  './styles/index.css';
+// log(foo);
+
+import foo from './foo.js'
+console.log(foo);
---
+++ webpack/src/testExport.js
@@ -0,0 +1,4 @@
+// const exportDependencies = require('./exportDependencies')
+// console.log(exportDependencies('./src/index.js'))
+const exportGraph = require('./exportGraph')
+console.log(exportGraph('./src/index.js'))
